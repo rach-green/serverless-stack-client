306cbd8a44a70afa37b10eef926f59da62063883
Rachel Green <rachel.green@rachel.green-MBPi>
2021-06-10 11:19:44 -0700

    Setting up our React app

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Tahoe Buddy</h1>
+        <p className="text-muted">unsure what to do? we have your back</p>
+      </div>
+    </div>
+  );
+}
